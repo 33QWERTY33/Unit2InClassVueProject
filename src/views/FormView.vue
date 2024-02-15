10def6b43cb82ac80dee2f5332d6cb51f71e9843
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-02-14 19:34:09 -0500

    fixed form view

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -3,6 +3,8 @@
     <div>
         <input v-model="input1" placeholder="input 1">
         <input v-model="input2" placeholder="input 2">
+        <div> {{ input1 }}</div>
+        <div> {{ input2 }}</div>
     </div>
 </template>
 
